3050ab53cced768c8a9c193e57664ee3c6b3a246
notaika <ercamanalo@gmail.com>
2024-09-08 11:43:38 -0700

    visual edits

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,16 +3,21 @@ import './Landing.scss';
 
 export default function Landing() {
   return (
-    <section className="main">
-      <div className="main__container">
-        <div className="main__text">
-          <h2 className="main__text-slogan">Elevate Your Essence.</h2>
-          <p className="main__text-description">Experience the perfect blend of nature and science with Evora. Our products nourish your hair, skin, and body, bringing out your natural beauty with every use.</p>
+    <main className="main">
+      <section className="hero">
+        <div className="hero__container">
+          <div className="hero__text">
+            <h2 className="hero__text-slogan">Elevate Your Essence.</h2>
+            <p className="hero__text-description">Experience the perfect blend of nature and science with Evora. Our products nourish your hair, skin, and body, bringing out your natural beauty with every use.</p>
+          </div>
+          <div className="hero__model">
+            <img src="src/assets/images/model.jpg" alt="Skin care model." className="hero__model-img"/>
+          </div>
         </div>
-        <div className="main__model">
-          <img src="src/assets/images/model.jpg" alt="Skin care model." className="main__model-img"/>
-        </div>
-      </div>
-    </section>
+      </section>
+      <section className="popular">
+        <h1 className="popular__heading">Most Popular Products</h1>
+      </section>
+    </main>
   )
 }
