af6db98a32c274f4fd0aa3d2b3f149d8554dc8c3
notaika <ercamanalo@gmail.com>
2024-09-08 10:08:54 -0700

    added model and slogan

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,8 +3,16 @@ import './Landing.scss';
 
 export default function Landing() {
   return (
-    <div>
-      Hi
-    </div>
+    <section className="main">
+      <div className="main__container">
+        <div className="main__text">
+          <h2 className="main__text-slogan">Elevate Your Essence.</h2>
+          <p className="main__text-description">Experience the perfect blend of nature and science with Evora. Our products nourish your hair, skin, and body, bringing out your natural beauty with every use.</p>
+        </div>
+        <div className="main__model">
+          <img src="src/assets/images/model.jpg" alt="Skin care model." className="main__model-img"/>
+        </div>
+      </div>
+    </section>
   )
 }
